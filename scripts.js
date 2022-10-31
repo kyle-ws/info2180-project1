21a4c8870860e146510e69b6244b482b37c8c4c6
kstar234 <32802115+kstar234@users.noreply.github.com>
2022-10-30 23:21:47 -0500

    Adding images and finishing final edits

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+
+
+document.addEventListener('submit',(event)=> {
+    event.preventDefault();
+
+    let emailA= document.getElementById('email').value;
+
+if(emailA=='' || emailA==null){
+    document.getElementsByClassName('message')[0].innerText="Please Enter a valid email address";
+    }
+else
+    document.getElementsByClassName('message')[0].innerText="Thank you! Your email address "+emailA+" has been added to our mailing system!";
+
+    
+})
